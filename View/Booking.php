f905dbad15373689f26d5ad2fcb3eefd84d86ec1
Saima <saimabinidris@gmail.com>
2026-08-20 11:34:19 +0600

    Booking UI

---
+++ View/Booking.php
@@ -0,0 +1,79 @@
+<html>
+    <head>
+        <title>Booking</title>
+        <link rel="stylesheet" href="./booking.css">
+    </head>
+    <body>
+        <form action="" method="post">
+            <div class="seats">
+                <section class="left-col">
+                    <section class="row">
+                        <input type="checkbox" name="a1" id="a1">
+                        <label for="a1">A1</label>
+                        <input type="checkbox" name="a2" id="a2">
+                        <label for="a2">A2</label>
+                    </section>
+                    <section class="row">
+                        <input type="checkbox" name="b1" id="b1">
+                        <label for="b1">B1</label>
+                        <input type="checkbox" name="b2" id="b2">
+                        <label for="b2">B2</label>
+                    </section>
+                    <section class="row">
+                        <input type="checkbox" name="c1" id="c1">
+                        <label for="c1">C1</label>
+                        <input type="checkbox" name="c2" id="c2">
+                        <label for="c2">C2</label>
+                    </section>
+                    <section class="row">
+                        <input type="checkbox" name="d1" id="d1">
+                        <label for="d1">D1</label>
+                        <input type="checkbox" name="d2" id="d2">
+                        <label for="d2">D2</label>
+                    </section>
+                    <section class="row">
+                        <input type="checkbox" name="e1" id="e1">
+                        <label for="e1">E1</label>
+                        <input type="checkbox" name="e2" id="e2">
+                        <label for="e2">E2</label>
+                    </section>
+                </section>
+
+                <section class="right-col">
+                    <section class="row">
+                        <input type="checkbox" name="a3" id="a3">
+                        <label for="a3">A3</label>
+                        <input type="checkbox" name="a4" id="a4">
+                        <label for="a4">A4</label>
+                    </section>
+                    <section class="row">
+                        <input type="checkbox" name="b3" id="b3">
+                        <label for="b3">B3</label>
+                        <input type="checkbox" name="b4" id="b4">
+                        <label for="b4">B4</label>
+                    </section>
+                    <section class="row">
+                        <input type="checkbox" name="c3" id="c3">
+                        <label for="c3">C3</label>
+                        <input type="checkbox" name="c4" id="c4">
+                        <label for="c4">C4</label>
+                    </section>
+                    <section class="row">
+                        <input type="checkbox" name="d3" id="d3">
+                        <label for="d3">D3</label>
+                        <input type="checkbox" name="d4" id="d4">
+                        <label for="d4">D4</label>
+                    </section>
+                    <section class="row">
+                        <input disabled type="checkbox" name="e3" id="e3">
+                        <label for="e3">E3</label>
+                        <input type="checkbox" name="e4" id="e4">
+                        <label for="e4">E4</label>
+                    </section>
+                </section>
+            </div>
+            <input type="submit" value="Book Seats">
+            
+        </form>
+    </body>
+</html>
